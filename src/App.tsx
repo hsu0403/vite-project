145a5e194cd1dfce0a2b2bdb63754fc3ecb424f6
SeongUn <hseongun0403@gmail.com>
2022-12-05 20:43:51 +0900

    vite react setup

---
+++ src/App.tsx
@@ -0,0 +1,8 @@
+import Welcome from "./components/Welcome";
+import "./css/main.css";
+
+function App() {
+  return <Welcome />;
+}
+
+export default App;
---
+++ src/components/Welcome.tsx
@@ -0,0 +1,8 @@
+export default function Welcome() {
+  console.log(import.meta.env);
+  return (
+    <div>
+      <h1>Hello</h1>
+    </div>
+  );
+}
